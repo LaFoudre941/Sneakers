e00db30043dd549fe07b88ba546fb5cfd562b7af
Andre9220 <96999666+Andre9220@users.noreply.github.com>
2023-07-13 12:06:25 +0100

    fix logout

--- Vue/navbar.php
+++ Vue/navbar.php
@@ -1,14 +1,12 @@
 <?php
     if (isset($_POST['sedeconnecter'])) {
-   
         $unControleur->logout();
     }
-
 ?>
 
 <div id="main-content">
-<nav>
-    <ul>
+    <nav>
+        <ul>
             <li><a href="index.php"><img src="./Vue/images/logo.png" alt="logo"></a></li>
             <li><a href="buying.php">Buying</a></li>
             <li><a href="youraccount.php">Your account</a></li>
@@ -16,23 +14,20 @@
             <li><a href="sell.php">Sell</a></li>
             <li><a href="cart.php">Cart</a></li>
             <form action="/search" method="GET">
-                <input name ="sedeconnecter" style="width: 250px;" type="text" name="q" placeholder="Search on YOUR MARKET">
+                <input style="width: 250px;" type="text" name="q" placeholder="Search on YOUR MARKET">
             </form>
-        <?php
-        // Ajouter la condition pour afficher le lien LOG IN ou LOG OUT
-        if (isset($_SESSION['email'])) {
-            echo '  <li>
-                    <form method="POST" action="'.htmlspecialchars($_SERVER['PHP_SELF']).'">
-                    <button class="btndeconnexion" type="submit">LOG OUT</button>
-                    </form>
-                    </li>';
-        } else {
-            echo '<li><a href="register.php" id="register-button">REGISTER</a></li>
-            <li><a href="connexion.php" id="login-button">LOG IN</a></li>';
-        }
-        ?>
-
-
-    </ul>
-</nav>
+            <?php
+            if (isset($_SESSION['email'])) {
+                echo '  <li>
+                        <form method="POST" action="'.htmlspecialchars($_SERVER['PHP_SELF']).'">
+                        <button class="btndeconnexion" type="submit" name="sedeconnecter">LOG OUT</button>
+                        </form>
+                        </li>';
+            } else {
+                echo '<li><a href="register.php" id="register-button">REGISTER</a></li>
+                <li><a href="connexion.php" id="login-button">LOG IN</a></li>';
+            }
+            ?>
+        </ul>
+    </nav>
 </div>
